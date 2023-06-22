73f076b9c58c982a95ba872ef17517efc2504cbe
L <Luen@users.noreply.github.com>
2023-06-22 14:55:59 +1000

    Added no longer working message.

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Leaflet BOM Radar
 
 This Leaflet plugin adds the BOM (Bureau of Meteorology) storm radar over the map to show the direction of incoming rain clouds. 
-
-Currently set to static as the looping tites has a bug when changing zoom levels. 
-
 Tiles from BOM weather https://weather.bom.gov.au/
+
+![Maintenance](https://img.shields.io/maintenance/yes/2022?style=for-the-badge)
+
+# No longer working
+
+Currently set to static as the looping tiles has a bug when changing zoom levels. 
+
+weather.bom.gov.au shutdown. 
+
+URL changed from 
+to: https://radar-tiles.service.bom.gov.au/tiles/{time}/{z}/{x}/{y}.png
+https://tilecache.rainviewer.com/v2/radar/{time}/256/{z}/{x}/{y}/{index}/1_0.png
+
+Also see https://www.weatherzone.com.au/ with information supplied by Weatherzone based on data provided by the Bureau of Meteorology (BOM)
+https://gs.weatherzone.com.au/cog-server/api/radar-bom-au/reflectivity-310/tile/web-mercator-512/5/17/28/radar-dbz-wz-ramp/png?timestamp=2023-06-22T04%3A30%3A00Z
--- leaflet.bom.js
+++ leaflet.bom.js
@@ -1,158 +1,131 @@
 /**
   * Leaflet.BOM
   * A JavaScript library for using BOM's layers and data for leaflet based maps.
-  * Project page: https://github.com/Luen/
+  * Project page: https://github.com/Luen/leaflet-bom-radar
+  * Data: https://weather.bom.gov.au/
   * Save to KML requires tokml.js, Save to GPX requires togpx.js
   * License: CC0 (Creative Commons Zero), see https://creativecommons.org/publicdomain/zero/1.0/
   */
 
-var coeff = 1000 * 60 * 10; // every 10 min
-var timeout = 500; // frame change every 3 secs
-var frameCount = 10; // 6 instances of images shown on bom site
-var run = true;
-var radarLayers = [];
-var times = [];
-var info;
+const coeff = 1000 * 60 * 10; // every 10 min
+const timeout = 500; // frame change every 500ms
+const frameCount = 10; // 6 instances of images shown on bom site
+let run = true;
+const radarLayers = [];
+const times = [];
+let info;
+
+const getRadarTime = i => {
+  let x = 1;
+  if (Number(new Date().getUTCMinutes().toString().slice(-1)) <= 5) {
+    x = 0;
+  }
+  const date = new Date(new Date() - (coeff * ((frameCount - x) - i)));
+  const year = date.getUTCFullYear().toString();
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = new Date(Math.floor(date.getTime() / coeff) * coeff).getUTCMinutes().toString().padStart(2, '0');
+  return `${year}${month}${day}${hours}${minutes}`;
+}
+
 L.TileLayer.bomRadar = L.TileLayer.extend({
 	options: {
-    //maxNativeZoom: 10,
-		//baseUrl: "https://api.weather.bom.gov.au/v1/rainradar/tiles/{time}/{z}/{x}/{y}.png",
-		attribution: 'Weather from <a href="https://weather.bom.gov.au/">BOM</a>',
-    id: "bom"
+		attribution: 'Weather from <a href="https://weather.bom.gov.au/">BOM</a>'
 	},
-  onAdd: function(map) {
-
-    /*const radarLocations = [[-35.661387, 149.512229],[-33.700764, 151.209470],[-29.620633, 152.963328],[-29.496994, 149.850825],[-31.024219, 150.192037],[-32.729802, 152.025422],[-29.038524, 167.941679],[-35.158170, 147.456307],[-34.262389, 150.875099],[-37.855210, 144.755512],[-34.234354, 142.086133],[-37.887532, 147.575475],[-35.990000, 142.010000],[-36.029663, 146.022772],[-19.885737, 148.075693],[-27.717739, 153.240015],[-16.818145, 145.662895],[-23.549558, 148.239166],[-23.855056, 151.262567],[-25.957342, 152.576898],[-23.439783, 144.282270],[-21.117243, 149.217213],[-27.606344, 152.540084],[-16.670000, 139.170000],[-20.711204, 139.555281],[-19.419800, 146.550974],[-26.440193, 147.349130],[-12.666413, 141.924640],[-16.287199, 149.964539],[-34.617016, 138.468782],[-43.112593, 147.805241],[-41.179147, 145.579986],[-23.795064, 133.888935],[-12.455933, 130.926599],[-12.274995, 136.819911],[-14.510918, 132.447010],[-11.648500, 133.379977],[-34.941838, 117.816370],[-17.948234, 122.235334],[-24.887978, 113.669386],[-20.653613, 116.683144],[-31.777795, 117.952768],[-33.830150, 121.891734],[-28.804648, 114.697349],[-25.033225, 128.301756],[-30.784261, 121.454814],[-22.103197, 113.999698],[-33.096956, 119.008796],[-32.391761, 115.866955],[-20.371845, 118.631670],[-30.358887, 116.305769],[-15.451711, 128.120856],[-35.329531, 138.502498],[-32.129823, 133.696361],[-37.747713, 140.774605],[-31.155811, 136.804400],[-18.228916, 127.662836],[-29.96, 146.81]];
-    for (i = 0; i < radarLocations.length; i++) {
-      L.circle(radarLocations[i]).bindTooltip("Radar location").addTo(map);
-    }*/
-
-    // Setup
-    run = true;
-    for (i = 0; i < frameCount; i++) {
-    	var time = getRadarTime(i);
-      //console.log(i+" "+time);
-    	times.push(time);
-      //times[i] = time;
-    	radarLayers[i] = L.tileLayer("https://api.weather.bom.gov.au/v1/rainradar/tiles/"+time+"/{z}/{x}/{y}.png", {
-    		layers: time,
-    		format: 'image/png',
-    		transparent: true,
-    		opacity: 0.0,
-        minNativeZoom: 5,
-        maxNativeZoom: 10
-    	});
-      radarLayers[i].addTo(map).bringToFront();
-    }
-
-    // Add Date Control
-    info = L.control.radarinfo({ position: 'bottomright' });
-    info.addTo(map);
-
-    function getRadarTime(i) {
-      var x = 1;
-      if (Number(new Date().getUTCMinutes().toString().slice(-1)) <= 5) { // BOM image isn't available straight away so wait 5 min.
-        //console.log(Number(new Date().getUTCMinutes().toString().slice(-1)));
-        //console.log("x=0");
-        x = 0;
-      }
-      var date = new Date(new Date()-(coeff*((frameCount-x)-i)));
-      var year = date.getUTCFullYear().toString();
-      var month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-      var day = date.getUTCDate().toString().padStart(2, '0');
-      var hours = date.getUTCHours().toString().padStart(2, '0');
-      var minutes = new Date(Math.floor(date.getTime() / coeff) * coeff).getUTCMinutes().toString().padStart(2, '0');
-      return year+month+day+hours+minutes;
-    }
-
-    function updateRadar(i) {
-      var time = getRadarTime(i);
-      //console.log("Updated frame "+i+" ("+times[i]+") to "+time);
-      times[i] = time;
-      //console.log(times);
-      radarLayers.map(function(layer){ layer.setOpacity(0)});
-      radarLayers[i] = L.tileLayer("https://api.weather.bom.gov.au/v1/rainradar/tiles/"+time+"/{z}/{x}/{y}.png", {
+	onAdd: map => {
+		// Setup
+		run = true;
+		for (let i = 0; i < frameCount; i++) {
+			const time = getRadarTime(i);
+			times.push(time);
+      //radarLayers[i] = L.tileLayer(`https://tilecache.rainviewer.com/v2/radar/${time}/256/{z}/{x}/{y}/1/1_0.png`, {
+      //radarLayers[i] = L.tileLayer(`https://api.weather.bom.gov.au/v1/rainradar/tiles/${time}/{z}/{x}/{y}.png`, {
+      radarLayers[i] = L.tileLayer(`https://radar-tiles.service.bom.gov.au/tiles/${time}/{z}/{x}/{y}.png`, {
         layers: time,
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.8,
-        minNativeZoom: 5,
-        maxNativeZoom: 10
-      });
-      radarLayers[i].addTo(map).bringToFront();
-    }
+				format: 'image/png',
+				transparent: true,
+				opacity: 0.0,
+				minNativeZoom: 5,
+				maxNativeZoom: 10
+			});
+			radarLayers[i].addTo(map).bringToFront();
+		}
 
-    var i = 0;
-    function radarLoop() { // create a loop function
-      setTimeout(function () { // call a 3s setTimeout when the loop is called
-        if (run) {
-          radarLayers.map(function(layer){ layer.setOpacity(0)});
-          radarLayers[i].setOpacity(0.8);
-          info._update(i);
-          //console.log("frame: "+(i)+"; time: "+times[i].slice(-4));
-          if (getRadarTime(i) != times[i]) {
-            updateRadar(i);
-          }
-          i++; // increment the counter
-          if (i == frameCount) {
-            i = 0;
-          }
-          radarLoop();
-        }
-      }, timeout)
-      return i;
-    }
-    radarLoop(); // start it
+		// Add Date Control
+		info = L.control.radarinfo({ position: 'bottomright' });
+		info.addTo(map);
+
+		const updateRadar = i => {
+			const time = getRadarTime(i);
+			times[i] = time;
+			radarLayers.map(layer => layer.setOpacity(0));
+			radarLayers[i] = L.tileLayer(`https://api.weather.bom.gov.au/v1/rainradar/tiles/${time}/{z}/{x}/{y}.png`, {
+				layers: time,
+				format: 'image/png',
+				transparent: true,
+				opacity: 0.8,
+				minNativeZoom: 5,
+				maxNativeZoom: 10
+			});
+			radarLayers[i].addTo(map).bringToFront();
+		}
+
+		let i = 0;
+		const radarLoop = () => { // create a loop function
+			setTimeout(() => {
+				if (run) {
+					radarLayers.map(layer => layer.setOpacity(0));
+					radarLayers[i].setOpacity(0.8);
+					info._update(i);
+					if (getRadarTime(i) !== times[i]) {
+						updateRadar(i);
+					}
+					i++;
+					if (i === frameCount) {
+						i = 0;
+					}
+					radarLoop();
+				}
+			}, timeout);
+		}
+		radarLoop();
 	},
-	onRemove: function(map) {
-    run = false;
-    radarLayers.map(function(layer){ layer.setOpacity(0)}); //.remove()???
-
-    info.remove();
+	onRemove: map => {
+		run = false;
+		radarLayers.map(layer => layer.setOpacity(0));
+		info.remove();
 	}
 });
-L.tileLayer.bomradar = function(map) {
-    return new L.TileLayer.bomRadar(map);
-}
+
+L.tileLayer.bomradar = map => new L.TileLayer.bomRadar(map);
 
 L.Control.radarInfo = L.Control.extend({
-    onAdd: function (map) {
-        this.container = L.DomUtil.create('div', 'leaflet-radar-control');
-        let slider_div = L.DomUtil.create('div', 'leaflet-radar-slider', this.container);
+  onAdd: function (map) {
+      this._container = L.DomUtil.create('div', 'leaflet-radar-control');
+      let slider_div = L.DomUtil.create('div', 'leaflet-radar-slider', this._container);
 
+      this._slider = document.createElement('input');
+      this._slider.id = 'leaflet-radar-slider';
+      this._slider.type = 'range';
+      this._slider.min = 1;
+      this._slider.max = frameCount;
+      this._slider.disabled = true;
+      slider_div.appendChild(this._slider);
 
-        this.slider = document.createElement('input');
-        this.slider.id = 'leaflet-radar-slider';
-        this.slider.type = 'range';
-        this.slider.min = 1;
-        this.slider.max = frameCount;
-        this.slider.disabled = true;
+      this._info_div = L.DomUtil.create('div', 'leaflet-radar-info', this._container);
 
-        slider_div.appendChild(this.slider);
-
-        this.info_div = L.DomUtil.create('div', 'leaflet-radar-info', this.container);
-
-        return this.container;
-    },
-    _update: function (i) {
-    	//if (typeof i !== "undefined") {
-    		//var time = times[x].replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, '$4:$5');
-        var time = new Date(times[i].replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, '$2/$3/$1 $4:$5 UTC'));
-        //var current = new Date(getRadarTime(frameCount-1).replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, '$2/$3/$1 $4:$5 UTC'));
-        var current = new Date();
-        var delay = new Date(current-time);
-        var delay = ((delay.getUTCHours()*60)+delay.getUTCMinutes()).toString();
-        //console.log(times[i].replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, '$2/$3/$1 $4:$5 UTC'));
-        //time = time.getHours().toString().padStart(2, '0')+":"+time.getMinutes().toString().padStart(2, '0')+" GMT+1000";
-        time = time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-    		this.info_div.innerHTML = "Radar time: " +time+" (-"+delay+" min)";
-        this.slider.value = i+1;
-    	//}
-    },
-    onRemove: function(map) {
-        // Nothing to do here
-    }
+      return this._container;
+  },
+  _update: function (i) {
+      const time = new Date(times[i].replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, '$2/$3/$1 $4:$5 UTC'));
+      const current = new Date();
+      const delay = new Date(current - time);
+      const delayMinutes = ((delay.getUTCHours() * 60) + delay.getUTCMinutes()).toString();
+      const formattedTime = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      this._info_div.innerHTML = `Radar time: ${formattedTime} (-${delayMinutes} min)`; // Changed this.info_div to this._info_div
+      this._slider.value = i + 1; // Changed this.slider to this._slider
+  }
 });
-L.control.radarinfo = function(opts) {
-    return new L.Control.radarInfo(opts);
-}
+
+L.control.radarinfo = opts => new L.Control.radarInfo(opts);
